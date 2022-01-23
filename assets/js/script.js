5fdad030346971d93e9c5f22d7d245d60aa8689e
hthomp15 <hailethomp@gmail.com>
2022-01-23 15:42:00 -0500

    add remaining js and comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 var options = {weekday: "long", month: "long", day: "numeric"};
 var dt = new Date();
 var displayDate= dt.toLocaleDateString("en-US", options);
+// Displays my date as specified in the header
 $("#currentDay").append(displayDate)
 
 var getTime = ()=> {
@@ -10,16 +11,19 @@ var getTime = ()=> {
 
     //loops through time block
     $(".time-block").each(function() {
-        var timeBlock = parseInt($(this).attr("id").split("hour-")[1]);
-        console.log(currentTime, timeBlock);
-        console.log($(this))
 
-        //checks if current hour is more than block hour
+        // This gets the value of my timeblock 
+        var timeBlock = parseInt($(this).attr("id"));
+        // console.log(currentTime, timeBlock);
+        // console.log($(this))
+
+        // checks if current hour equals block hour
+        // if true add the class and remove other classes
         if (currentTime === timeBlock) {
             $(this).addClass("present");
             $(this).removeClass("past future");
         } 
-        //checks if current hour equals block hour
+        //checks if current hour is less than the block hour
         else if (currentTime < timeBlock) {
             $(this).addClass("future");
             $(this).removeClass("past present")
@@ -37,24 +41,26 @@ var onStart = ()=> {
 
     //adds click listener function to saveBtn 
     $(".saveBtn").on("click", function(){
+        // Find value of the textbox
         var text = $(this).siblings(".event").val();
+        // Find the time at the row 
         var time = $(this).parent().attr("id");
         // console.log(text, time);
-
+        // Send time and textbox input to local storage
         localStorage.setItem(time, text); 
            
     })
 
     //returns value of the localStorage item
-    $("#hour-9 .event").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .event").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .event").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .event").val(localStorage.getItem("hour-12"));
-    $("#hour-13 .event").val(localStorage.getItem("hour-13"));
-    $("#hour-14 .event").val(localStorage.getItem("hour-14"));
-    $("#hour-15 .event").val(localStorage.getItem("hour-15"));
-    $("#hour-16 .event").val(localStorage.getItem("hour-16"));
-    $("#hour-17 .event").val(localStorage.getItem("hour-17"));
+    $("#9 .event").val(localStorage.getItem("9"));
+    $("#10 .event").val(localStorage.getItem("10"));
+    $("#11 .event").val(localStorage.getItem("11"));
+    $("#12 .event").val(localStorage.getItem("12"));
+    $("#13 .event").val(localStorage.getItem("13"));
+    $("#14 .event").val(localStorage.getItem("14"));
+    $("#15 .event").val(localStorage.getItem("15"));
+    $("#16 .event").val(localStorage.getItem("16"));
+    $("#17 .event").val(localStorage.getItem("17"));
 
 }
 
