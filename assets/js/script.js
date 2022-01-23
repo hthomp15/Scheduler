37cf362a3ceec3a8f57d12b322b59725cb0fc572
hthomp15 <hailethomp@gmail.com>
2022-01-23 15:30:10 -0500

    add get time function to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,31 @@ var dt = new Date();
 var displayDate= dt.toLocaleDateString("en-US", options);
 $("#currentDay").append(displayDate)
 
+var getTime = ()=> {
+    //gets the current time
+    var currentTime = new Date().getHours();
 
+    //loops through time block
+    $(".time-block").each(function() {
+        var timeBlock = parseInt($(this).attr("id").split("hour-")[1]);
+        console.log(currentTime, timeBlock);
+        console.log($(this))
+
+        //checks if current hour is more than block hour
+        if (currentTime === timeBlock) {
+            $(this).addClass("present");
+            $(this).removeClass("past future");
+        } 
+        //checks if current hour equals block hour
+        else if (currentTime < timeBlock) {
+            $(this).addClass("future");
+            $(this).removeClass("past present")
+        }
+        else {
+            $(this).addClass("past");
+            $(this).removeClass("present future");
+        }
+    })
+
+}
 
